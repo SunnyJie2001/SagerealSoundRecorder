81218b7057f2f9c21af6aa24fb1cfcde4b3dad7e
13668461988 <1454549340@qq.com>
2022-12-12 23:16:00 +0800

    绘制跳转设置界面，已经增加设置图标

---
+++ app/src/main/java/com/sagereal/sagerealsoundrecorder/setting/SettingActivity.java
@@ -0,0 +1,95 @@
+package com.sagereal.sagerealsoundrecorder.setting;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sagereal.sagerealsoundrecorder.databinding.ActSettingBinding;
+
+
+/**
+ * @version 1.0
+ * @anthur ssj
+ * @date 2022/12/12 22:05
+ */
+public class SettingActivity extends AppCompatActivity {
+    private ActSettingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActSettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        boolean soundMode = getSharedPreferences("config", MODE_PRIVATE).getBoolean("soundMode", false);
+        binding.swTT.setChecked(soundMode);
+        binding.swTT.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                getSharedPreferences("config", MODE_PRIVATE).edit().putBoolean("soundMode", isChecked).commit();
+            }
+        });
+        boolean rename = getSharedPreferences("config", MODE_PRIVATE).getBoolean("rename", false);
+        binding.swName.setChecked(rename);
+        binding.swName.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                getSharedPreferences("config", MODE_PRIVATE).edit().putBoolean("rename", isChecked).commit();
+            }
+        });
+        binding.versi.setText(getapkname());
+
+        String voiceType = getSharedPreferences("config", MODE_PRIVATE).getString("voiceType", "amr");
+
+        binding.type.setText(voiceType);
+
+        binding.rlVoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VoiceTypeDialog voiceTypeDialog = new VoiceTypeDialog(SettingActivity.this);
+                voiceTypeDialog.setDialogWidth();
+                voiceTypeDialog.show();
+            }
+        });
+
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        String voiceType = getSharedPreferences("config", MODE_PRIVATE).getString("voiceType", "amr");
+
+        binding.type.setText(voiceType);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        String voiceType = getSharedPreferences("config", MODE_PRIVATE).getString("voiceType", "amr");
+
+        binding.type.setText(voiceType);
+    }
+
+    private String getapkname() {
+
+        PackageManager packagemanager = getPackageManager();
+
+
+        PackageInfo packinfo = null;
+        try {
+            packinfo = packagemanager.getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String version = packinfo.versionName;
+
+        return "V" + version;
+
+    }
+}
---
+++ app/src/main/java/com/sagereal/sagerealsoundrecorder/setting/VoiceTypeDialog.java
@@ -0,0 +1,73 @@
+package com.sagereal.sagerealsoundrecorder.setting;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.NonNull;
+
+import com.sagereal.sagerealsoundrecorder.R;
+import com.sagereal.sagerealsoundrecorder.databinding.DialogVoicetypeBinding;
+
+/**
+ * @version 1.0
+ * @anthur ssj
+ * @date 2022/12/12 22:06
+ */
+public class VoiceTypeDialog extends Dialog implements View.OnClickListener{
+    public VoiceTypeDialog(@NonNull Context context) {
+        super(context);
+    }
+
+    DialogVoicetypeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DialogVoicetypeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.tvMc.setOnClickListener(this);
+        binding.tvSj.setOnClickListener(this);
+        binding.tvDx.setOnClickListener(this);
+    }
+    public void setDialogWidth(){
+        //获取当前窗口对象
+        Window window = getWindow();
+        //获取窗口信息
+        WindowManager.LayoutParams wlp = window.getAttributes();
+        //获取屏幕宽度
+        Display display = window.getWindowManager().getDefaultDisplay();
+        wlp.width = display.getWidth()-30;
+        wlp.gravity = Gravity.CENTER;
+        //设置窗口背景透明
+        window.setBackgroundDrawableResource(android.R.color.transparent);
+        //设置窗口参数
+        window.setAttributes(wlp);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tv_mc:
+                getContext().getSharedPreferences("config", MODE_PRIVATE).edit().putString("voiceType", "aac").commit();
+                break;
+            case R.id.tv_sj:
+                getContext().getSharedPreferences("config", MODE_PRIVATE).edit().putString("voiceType", "mp3").commit();
+                break;
+            case R.id.tv_dx:
+                getContext().getSharedPreferences("config", MODE_PRIVATE).edit().putString("voiceType", "amr").commit();
+                break;
+        }
+        Intent intent = new Intent(getContext(), SettingActivity.class);
+        getContext().startActivity(intent);
+        dismiss();
+    }
+}
